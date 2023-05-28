81bb6903ca9e7a6dc0188442b6bdd0de1fe79edd
SmailikHappy <SmailikHappy@users.noreply.github.com>
2023-05-28 12:54:19 +0200

    [FIX] Computer player can swipe the menu
    + Swipe via mouse can be done
    - MenuSwiper class deleted, as it is not used
    ~ Scripts have been sorted into the folders

--- Assets/Scripts/Menu/MenuSwipe.cs
+++ Assets/Scripts/Menu/MenuSwipe.cs
@@ -6,9 +6,9 @@ using UnityEngine.EventSystems;
 public class MenuSwipe : MonoBehaviour
 {
     private RectTransform menuLocation; 
-    private Vector2 lastTouchPos;
+    private Vector2 lastPointerPos;
     private bool isPressed = false;
-    private Vector2 startTouchPos;
+    private Vector2 startPointerPos;
     [SerializeField] Collider2D startCollider;
     [SerializeField] Collider2D endCollider;
     [SerializeField] Camera cameraUI;
@@ -21,17 +21,31 @@ public class MenuSwipe : MonoBehaviour
 
     void Update()
     {
-        if (Input.touchCount == 1) {
+        // gets input from touch
+        if (Input.touchCount == 1)
+        {
             Touch touch = Input.GetTouch(0);
             if (!isPressed) {
                 isPressed = true;
-                startTouchPos = cameraUI.ScreenToWorldPoint(new Vector2 (touch.position.x, touch.position.y));
+                startPointerPos = cameraUI.ScreenToWorldPoint(new Vector2 (touch.position.x, touch.position.y));
             }
-            lastTouchPos = cameraUI.ScreenToWorldPoint(new Vector2 (touch.position.x, touch.position.y));
-        } else {
+            lastPointerPos = cameraUI.ScreenToWorldPoint(new Vector2 (touch.position.x, touch.position.y));
+        }
+        // gets input from mouse
+        else if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Mouse pressed");
+            if (!isPressed) {
+                isPressed = true;
+                startPointerPos = cameraUI.ScreenToWorldPoint(new Vector2 (Input.mousePosition.x, Input.mousePosition.y));
+            }
+            lastPointerPos = cameraUI.ScreenToWorldPoint(new Vector2 (Input.mousePosition.x, Input.mousePosition.y));
+        }
+        else
+        {
             if (isPressed) {
                 isPressed = false;
-                if (startCollider.OverlapPoint(startTouchPos) && endCollider.OverlapPoint(lastTouchPos)) {
+                if (startCollider.OverlapPoint(startPointerPos) && endCollider.OverlapPoint(lastPointerPos)) {
                     SwipeToBook();
                 }
             }
